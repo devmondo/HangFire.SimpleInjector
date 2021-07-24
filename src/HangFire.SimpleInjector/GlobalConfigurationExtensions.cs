f5e7690c2c7e61d7181b1f30f27b8dfe3df41f61
Paulo Leao <opaulodiogo@gmail.com>
2021-07-24 01:06:09 -0300

    including extension method to improve configuration

---
+++ src/HangFire.SimpleInjector/GlobalConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire.Annotations;
+using SimpleInjector;
+using System;
+
+namespace Hangfire.SimpleInjector
+{
+    public static class GlobalConfigurationExtensions
+    {
+        public static IGlobalConfiguration<SimpleInjectorJobActivator> UseSimpleInjectorActivator([NotNull] this IGlobalConfiguration configuration,
+            [NotNull] Container container)
+        {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            if (container == null)
+            {
+                throw new ArgumentNullException(nameof(container));
+            }
+
+            return configuration.UseActivator(new SimpleInjectorJobActivator(container));
+        }
+    }
+}
